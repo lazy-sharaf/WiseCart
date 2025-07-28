3950032a3d3002d1fee85b76e10bc565d297ea82
Jowel <jowel2305341452@diu.edu.bd>
2025-07-28 22:00:11 +0600

    Minor Change
    Minor Change

--- wisecart/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'products'
-
-urlpatterns = [
-    path('product/<str:store_name>/<path:product_url>/', views.product_detail, name='product_detail'),
-]
